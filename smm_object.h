645ee305ae3da97c8df3058de397fd1c32c40f7b
sowon0627-byte <sowon0627@sookmyung.ac.kr>
2025-12-21 23:51:24 +0900

    Update smm_object.h

--- smm_object.h
+++ smm_object.h
@@ -31,31 +31,41 @@
 #define SMMNODE_OBJTYPE_FOOD    2
 #define SMMNODE_OBJTYPE_FEST    3
 
-/* grade :
-    A+,
-    A0,
-    A-,
-    B+,
-    B0,
-    B-,
-    C+,
-    C0,
-    C-
-*/
 
 #define SMMNODE_MAX_GRADE       13
+
+
+//structure type definition
+typedef enum {
+        SMM_GRADE_AP = 0,
+        SMM_GRADE_A0 = 1,
+        SMM_GRADE_AM = 2,
+        SMM_GRADE_BP = 3,
+        SMM_GRADE_B0 = 4,
+        SMM_GRADE_BM = 5,
+        SMM_GRADE_CP = 6,
+        SMM_GRADE_C0 = 7,
+        SMM_GRADE_CM = 8,
+        SMM_GRADE_DP = 9,
+        SMM_GRADE_D0 = 10,
+        SMM_GRADE_DM = 11,
+        SMM_GRADE_F = 12,
+} smmGrade_e;
+        
 
 //object generation
 void* smmObj_genObject(char* name, int objType, int type, int credit, int energy, int grade);
 char* smmObj_getObjectName(void *ptr);
-int smmObj_getNodeType(void *ptr);
+int smmObj_getObjectCredit(void *ptr);
 int smmObj_getObjectEnergy(void *ptr);
-char* smmObj_getTypeName(int node_type);
-int smmObj_getNodeCredit(int node_nr);
-//member retrieving
-
+int smmObj_getTypeName(void *ptr);
+int smmObj_getObjectGrade(void *ptr);
 
 //element to string
+char* smmObj_getGradeName(smmGrade_e grade);
+smmGrade_e takeLecture(int player, char *lectureName, int credit);
+
+
 
 
 
